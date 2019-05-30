e4a8ca1255b8645289f7bf8ab31b988089e4f5ec
aluebke19 <32465243+aluebke19@users.noreply.github.com>
2019-05-30 12:15:02 -0500

    Update Maze.py

--- Maze.py
+++ Maze.py
@@ -3,42 +3,132 @@ from setup import RPL
 import RoboPiLib as RPL
 sensor_pin = 5
 RPL.pinMode(sensor_pin,RPL.INPUT)
+def analogRead(pin):
+  putPacket(ANREAD, bytearray([5]), 1)
+  buff = getPacket()
+  return int(buff[3][1]) | (int(buff[3][2]) << 8)
 
-from marvelmind import MarvelmindHedge
-from time import sleep
-import sys
+while True:
+  if RPL.analogRead(5) > 450 and RPL.analogRead(3) > 450:
+    RPL.servoWrite(2,1490)
+    RPL.servoWrite(1,300)
+    print "ok"
+  if 400 > RPL.analogRead(5) and RPL.analogRead(3) > 250:
+    RPL.servoWrite(2,2000)
+    RPL.servoWrite(1,1470)
+    print "yeah"
+  if RPL.analogRead(5) >= 400 and RPL.analogRead(5) <= 450:
+    RPL.servoWrite(2,2000)
+    RPL.servoWrite(1,300)
+    print "gratata"
+  if RPL.analogRead(3) < 250:
+    RPL.servoWrite(2,2000)
+    RPL.servoWrite(1,0)
+  if RPL.digitalRead(17) == 0:
+    break
 
-def main():
-    hedge = MarvelmindHedge(tty = "/dev/ttyACM0", adr=None, debug=False) # create MarvelmindHedge thread
-    hedge.start() # start thread
-    while True:
-        try:
-            sleep(1)
-            # print (hedge.position()) # get last position and print
-            hedge.print_position()
-            if (hedge.distancesUpdated):
-				hedge.print_distances()
-        except KeyboardInterrupt:
-            hedge.stop()  # stop and close serial port
-            sys.exit()
+while True:
+  if RPL.digitalRead(17) == 0:
+    RPL.servoWrite(2,2000)
+    RPL.servoWrite(1,0)
+  if RPL.digitalRead(17) == 1 and RPL.analogRead(3) > RPL.analogRead(5):
+    break
 
-def drive():
-    if 1.5 > X and 1.5 > Y:
-        RPL.servoWrite(0,1000)  # drive straight
-        RPL.servoWrite(1,2000)
-    if X < 0 and 2 > Y > 0:
-        RPL.servoWrite(0,200)  # turn right
-        RPL.servoWrite(1,2000)
-    if 2 > X > 0 and Y < 0:
-        RPL.servoWrite(0,1000)    # turn left
-        RPL.servoWrite(1,200)
-    if 2 > X > 1.5 and 2 > Y > 1.5:
-        RPL.servoWrite(0,0)         # stop
-        RPL.servoWrite(1,0)
-    if X > 2 or Y > 2:
-        RPL.servoWrite(0,0)    # backwards
-        RPL.servoWrite(1,0)
+while True:
+  if RPL.analogRead(5) > 440 or RPL.analogRead(3) > 440:
+    RPL.servoWrite(2,1490)
+    RPL.servoWrite(1,300)
+    print "ok"
+  if RPL.analogRead(5) < 415 or RPL.analogRead(3) < 415:
+    RPL.servoWrite(2,2000)
+    RPL.servoWrite(1,1470)
+    print "yeah"
+  if RPL.analogRead(5) >= 420 or RPL.analogRead(5) <= 430:
+    RPL.servoWrite(2,2000)
+    RPL.servoWrite(1,300)
+    print "gratata"
+  if RPL.digitalRead(17) == 0:
+    break
 
-import threading
-threading.Thread(target = main, name = 'main').start()
-threading.Thread(target = drive, name = 'drive').start()
+while True:
+  if RPL.digitalRead(17) == 0:
+    RPL.servoWrite(2,0)
+    RPL.servoWrite(1,300)
+  if RPL.digitalRead(17) == 1 and RPL.analogRead(3) > 420:
+    break
+
+while True:
+  if RPL.analogRead(5) > 440 or RPL.analogRead(3) > 440:
+    RPL.servoWrite(2,1490)
+    RPL.servoWrite(1,300)
+    print "ok"
+  if RPL.analogRead(5) < 415 or RPL.analogRead(3) < 415:
+    RPL.servoWrite(2,2000)
+    RPL.servoWrite(1,1470)
+    print "yeah"
+  if RPL.analogRead(5) >= 420 or RPL.analogRead(5) <= 430:
+    RPL.servoWrite(2,2000)
+    RPL.servoWrite(1,300)
+    print "gratata"
+  if RPL.digitalRead == 0:
+    break
+
+while True:
+  if RPL.analogRead(5) > 440 or RPL.analogRead(3) > 440:
+    RPL.servoWrite(2,1480)
+    RPL.servoWrite(1,2000)
+    print "ok"
+  if RPL.analogRead(5) < 415 or RPL.analogRead(3) < 415:
+    RPL.servoWrite(2,500)
+    RPL.servoWrite(1,1520)
+    print "yeah"
+  if RPL.analogRead(5) >= 420 or RPL.analogRead(5) <= 430:
+    RPL.servoWrite(2,500)
+    RPL.servoWrite(1,2000)
+    print "gratata"
+  if RPL.analogRead(0) < 300:
+    break
+
+while True:
+  if RPL.analogRead(0) < 300:
+    RPL.servoWrite(2,0)
+    RPL.servoWrite(1,2000)
+  if RPL.analogRead(0) > 400:
+    break
+
+while True:
+  if RPL.analogRead(5) > 440 or RPL.analogRead(3) > 440:
+    RPL.servoWrite(2,1480)
+    RPL.servoWrite(1,2000)
+    print "ok"
+  if RPL.analogRead(5) < 415 or RPL.analogRead(3) < 415:
+    RPL.servoWrite(2,500)
+    RPL.servoWrite(1,1520)
+    print "yeah"
+  if RPL.analogRead(5) >= 420 or RPL.analogRead(5) <= 430:
+    RPL.servoWrite(2,500)
+    RPL.servoWrite(1,2000)
+    print "gratata"
+  if RPL.analogRead(5) < 300 and RPL.analogRead(3) > 300:
+    break
+
+while True:
+  if RPL.analogRead(5) < 300 and RPL.analogRead(3) > 300:
+    RPL.servoWrite(2,500)
+    RPL.servoWrite(1,0)
+  if RPL.analogRead(5) > RPL.analogRead(3):
+    break
+
+while True:
+  if RPL.analogRead(0) > 440:
+    RPL.servoWrite(2,1480)
+    RPL.servoWrite(1,2000)
+    print "ok"
+  if RPL.analogRead(0) < 415:
+    RPL.servoWrite(2,500)
+    RPL.servoWrite(1,1520)
+    print "yeah"
+  if RPL.analogRead(0) >= 420:
+    RPL.servoWrite(2,500)
+    RPL.servoWrite(1,2000)
+    print "gratata"
